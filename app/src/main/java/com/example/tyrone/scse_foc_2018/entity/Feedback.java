8d06acc9a754b116305bd983a01ef28eb2c40dfe
tyfecho <32788268+tyfecho@users.noreply.github.com>
2018-08-06 16:56:19 +0800

    feddback submit
    now can successfully submit to database

--- app/src/main/java/com/example/tyrone/scse_foc_2018/entity/Feedback.java
+++ app/src/main/java/com/example/tyrone/scse_foc_2018/entity/Feedback.java
@@ -2,25 +2,32 @@ package com.example.tyrone.scse_foc_2018.entity;
 
 public class Feedback extends Object {
 
-    private String uid, focDate, gameRating, welfareRating, hosRating, others;
+    private String uid, focDate, venue, service, reception, activities,duration,overall,feedback;
 
     public Feedback() {
         focDate="0";
         uid="uid";
-        gameRating="0";
-        welfareRating="0";
-        hosRating="0";
-        others="nil";
+        venue="0";
+        service="0";
+        reception="0";
+        activities="0";
+        duration="0";
+        overall="0";
+        feedback="nil";
     }
 
-    public Feedback(String in_uid, String in_focDate, String in_gameRating, String in_welfareRating, String in_hosRating, String in_others)
+    public Feedback(String in_uid, String in_focDate, String in_venue, String in_service, String in_reception, String in_activities, String in_duration, String in_overall, String in_feedback)
     {
         focDate = in_focDate;
         uid = in_uid;
-        gameRating = in_gameRating;
-        welfareRating = in_welfareRating;
-        hosRating = in_hosRating;
-        others = in_others;
+        venue=in_venue;
+        service=in_service;
+        reception=in_reception;
+        activities=in_activities;
+        duration=in_duration;
+        overall=in_overall;
+        feedback=in_feedback;
+
 
     }
 
@@ -28,23 +35,26 @@ public class Feedback extends Object {
         return focDate;
     }
 
-    public String getGameRating() {
-        return gameRating;
+    public String getUid() { return uid; }
+    public String getVenue() {
+        return venue;
     }
-
-    public String getUid() {
-        return uid;
+    public String getService() {
+        return service;
     }
-
-    public String getWelfareRating() {
-        return welfareRating;
+    public String getReception() {
+        return reception;
     }
-
-    public String getOthers() {
-        return others;
+    public String getActivities() {
+        return activities;
     }
-
-    public String getHosRating() {
-        return hosRating;
+    public String getDuration() {
+        return duration;
+    }
+    public String getOverall() {
+        return overall;
+    }
+    public String getFeedback() {
+        return feedback;
     }
 }
